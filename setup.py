c8bd12a35370cf9ca9275f29f3fe742365a2dfb2
Daehan Lim <penjan.eng@gmail.com>
2023-02-24 14:20:15 +0900

    Add license file

--- setup.py
+++ setup.py
@@ -2,18 +2,23 @@ from setuptools import setup
 
 setup(
     name='daehan_mlutil',
-    version='0.1.2',
+    version='0.1.3',
     description='A ML utilities package',
     url='https://github.com/daehan-lim/mlutil',
     author='Daehan Lim',
     author_email='daehanlim@g.cnu.ac.kr',
+    license='MIT License',
     packages=['daehan_mlutil'],
 
     classifiers=[
         'Development Status :: 1 - Planning',
-        'Intended Audience :: Science/Research',
+        'Intended Audience :: Science/Research/Developers',
+        'License :: OSI Approved :: MIT License',
         'Operating System :: POSIX :: Linux',        
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
     ],
